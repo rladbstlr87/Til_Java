19331ca69dc604831ca27611473d2df5c42f2616
rladbstlr87 <manylong@naver.com>
2025-08-20 22:56:01 +0900

    Constants

---
+++ src/chap_01/_06_Constants.java
@@ -0,0 +1,12 @@
+package chap_01;
+
+public class _06_Constants { // 상수
+    public static void main(String[] args) {
+        final String KR_COUNTRY_CODE = "+82"; // 국가 번호
+//        KR_COUNTRY_CODE = "+8282";
+        System.out.println(KR_COUNTRY_CODE);
+
+        final double PI = 3.141592; // 고정된 원주율
+        final String DATE_OF_BIRTH = "1987-09-20"; // 생년월일도 바뀔 수 없음
+    }
+}
