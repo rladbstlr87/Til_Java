ad3113763682b5dfc87b7adbd6372620ae0a166c
rladbstlr87 <manylong@naver.com>
2025-08-21 00:04:29 +0900

    비교 연산자

---
+++ src/chap_02/_03_Operator3.java
@@ -0,0 +1,19 @@
+package chap_02;
+
+public class _03_Operator3 {
+    public static void main(String[] args) {
+        // 비교 연산자
+
+        System.out.println(5 > 3); // 5는 3보다 크다(참이면 true, 거짓이면 false)
+        System.out.println(5 >= 3);
+        System.out.println(5 >= 5);
+        System.out.println(5 >= 7);
+
+        System.out.println(5 < 3);
+        System.out.println(5 <= 3);
+
+        System.out.println(5 == 5);
+        System.out.println(5 == 3);
+        System.out.println(5 != 3);
+    }
+}
